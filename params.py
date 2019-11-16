6e0a2b5f14ae970a44fd9eb0b5fe60534adbea96
iamhyc <sudofree@163.com>
2019-11-16 16:54:54 +0800

    update params; lots left todo today

--- params.py
+++ params.py
@@ -18,35 +18,73 @@ LQ    = 10 #maximum queue length on ES (inclusive)
 
 TS    = 0.01         #timeslot, 10ms
 TB    = 0.50         #interval, 500ms
-N_SLT = int(TB/TS)   #
-N_CNT = int(3*TB/TS) #maximum uploading time
+N_SLT = int(TB/TS)   #50 slots/interval
+N_CNT = 3*N_SLT      #(for convenience)
 
-UL_MIN    = 10*TS
-UL_MAX    = N_CNT*TS + 11*TS
-UL_RNG    = np.linspace(UL_MIN/TS , UL_MAX/TS, num=N_CNT+1, dtype=np.int32)
+BR_MIN   = int( 0.00 * N_SLT )    #(inclusive)
+BR_MAX   = int( 0.50 * N_SLT )    #(exclusive)
+BR_RNG   = np.arange(BR_MIN, BR_MAX,       step=1, dtype=np.int32)
+BR_RNG_L = len(BR_RNG)
 
-PROC_MIN  = 5/TS
-PROC_MAX  = 15/TS
-PROC_RNG  = np.arange(PROC_MIN, PROC_MAX, dtype=np.int32)
-PROC_RNG_L= len(PROC_RNG)
-DIM_P     = (LQ+1)*PROC_MAX
+UL_MIN   = int( 0.00 * N_SLT )    #(inclusive)
+UL_MAX   = int( 3.00 * N_SLT )    #(inclusive)NOTE: upper boundary included
+UL_RNG   = np.arange(UL_MIN, UL_MAX+1,     step=1, dtype=np.int32)
+UL_RNG_L = len(UL_RNG)
+assert(N_CNT == UL_RNG_L))        #(for convenience)
+
+PROC_MIN   = int( 1.00 * N_SLT )  #(inclusive)
+PROC_MAX   = int( 3.50 * N_SLT )  #(exclusive) 
+PROC_RNG   = np.arange(PROC_MIN, PROC_MAX, step=1, dtype=np.int32)
+PROC_RNG_L = len(PROC_RNG)
+DIM_P      = (LQ+1)*PROC_MAX
 
 npzfile = 'logs/{:05d}.npz'.format(RANDOM_SEED)
 
 @njit
 def genProcessingDistribution():
-    np.zeros((N_ES, N_JOB, PROC_RNG_L))
-    pass
+    dist = np.zeros((N_ES, N_JOB, PROC_RNG_L), dtype=np.float32)
+    #TODO:
+    return dist
 
 @njit
 def genUploadingDistribution():
-    pass
+    dist = np.zeros((N_AP, N_ES, N_JOB, UL_RNG_L), dtype=np.float32)
+    #TODO:
+    return dist
+
+@njit
+def genDelayDistribution():
+    dist = np.zeros((), dtype=np.float32)
+    #TODO:
+    return dist
+
+@njit
+def genTransitionMatrix(ul_prob):
+    ul_mat  = np.zeros((N_AP,N_ES,N_JOB), dtype=np.float32)
+    off_mat = np.zeros((N_AP,N_ES,N_JOB), dtype=np.float32)
+    #TODO:
+    return ul_mat, off_mat
 
 if Path(npzfile).exists():
-    _params = np.load(npzfile)
+    _params   = np.load(npzfile)
+    arr_prob  = _params['arr_prob']
+    br_dist   = _params['br_dist']
+    proc_dist = _params['proc_dist']
+    # ul_prob   = _params['ul_prob']
+    ul_trans  = _params['ul_trans']
+    off_trans = _params['off_trans']
 else:
-    # arr_prob
-    # ul_prob
-    # ul_dist
-    # ul_trans_mat
-    # 
+    arr_prob  = 0.05 + 0.05 * np.random.rand(N_AP, N_JOB).astype(np.float32)
+    ul_prob   = genUploadingDistribution()
+    br_dist   = genDelayDistribution()
+    proc_dist = genProcessingDistribution()
+    ul_trans, off_trans = genTransitionMatrix(ul_prob)
+
+    np.savez(npzfile, **{
+        'arr_prob' : arr_prob,
+        'ul_prob'  : ul_prob,
+        'proc_dist': proc_dist,
+        'ul_trans' : ul_trans,
+        'off_trans': off_trans
+    })
+    pass
