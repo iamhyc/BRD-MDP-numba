8bad5026986f1ea1e2d25eced37816d445aba37c
iamhyc <sudofree@163.com>
2019-11-30 19:02:41 +0800

    fix plot unc

--- plot-traces.py
+++ plot-traces.py
@@ -7,6 +7,7 @@ from sys import argv
 import glob
 
 npzfiles = glob.glob( '{LOG_DIR}/*.npz'.format(LOG_DIR=argv[1]) )
+npzfiles.sort()
 n_files  = len(npzfiles)
 
 mdp_trace     = list()
@@ -27,7 +28,7 @@ for i,_file in enumerate(npzfiles):
     })
     random_trace.append({
         'ap_stat': trace['random_ap_stat'],
-        'es_stat': trace['random_es_stat'],
+        'es_stat': trace['random_es_Stat'],
     })
     selfish_trace.append({
         'ap_stat': trace['selfish_ap_stat'],
@@ -41,10 +42,10 @@ def plot_cost_vs_time():
     random_cost  = [np.sum(x['ap_stat'])+np.sum(x['es_stat'][:,:,0]) for x in random_trace]
     selfish_cost = [np.sum(x['ap_stat'])+np.sum(x['es_stat'][:,:,0]) for x in selfish_trace]
 
-    plt.plot(range(n_files), mdp_cost, '-ro')
-    plt.plot(range(n_files), mdp_cost, '-bo')
-    plt.plot(range(n_files), mdp_cost, '-go')
-    plt.plot(range(n_files), mdp_cost, '-co')
+    plt.plot(range(n_files), mdp_cost,     '-ro')
+    plt.plot(range(n_files), sqf_cost,     '-go')
+    plt.plot(range(n_files), random_cost,  '-co')
+    plt.plot(range(n_files), selfish_cost, '-bo')
 
     plt.legend(['MDP Policy', 'Selfish Policy', 'SQF Policy', 'Random Policy'])
     plt.show()
@@ -60,25 +61,26 @@ def plot_cost_cdf_vs_time():
     ylim = max([arr.max() for arr in y])
     pmf_x = np.linspace(0, ylim, num=1000)
     pmf_y = np.zeros((5, 1000))
-    # inter_x     = np.linspace(0, ylim, num=200)
-    # inter_y_pmf = np.zeros((5, 200))
+    # pmf_inter_x     = np.linspace(0, ylim, num=200)
+    # pmf_inter_y = np.zeros((5, 200))
 
-    for i in range(5):
+    for i in range(4):
         for j in range(1,1000):
-            y_pmf[i][j] = np.logical_and( y[i]>=pmf_x[j-1], y[i]<pmf_x[j] ).sum()
-        # y_pmf[i] = np.cumsum(y_pmf[i]) / 1000
-        # interp_func = interp1d(x, y_pmf[i], kind='quadratic')
-        # inter_y_pmf[i] = interp_func(inter_x)
+            pmf_y[i][j] = np.logical_and( y[i]>=pmf_x[j-1], y[i]<pmf_x[j] ).sum()
+        pmf_y[i] = np.cumsum(pmf_y[i]) / 1000
+        # interp_func = interp1d(x, pmf_y[i], kind='quadratic')
+        # pmf_inter_y[i] = interp_func(pmf_inter_x)
         pass
 
     plt.xlim(0, ylim)
     plt.plot(pmf_x, pmf_y[0], '-r')
-    plt.plot(pmf_x, pmf_y[1], '-b')
-    plt.plot(pmf_x, pmf_y[2], '-g')
-    plt.plot(pmf_x, pmf_y[3], '-c')
+    plt.plot(pmf_x, pmf_y[1], '-g')
+    plt.plot(pmf_x, pmf_y[2], '-c')
+    plt.plot(pmf_x, pmf_y[3], '-b')
 
     plt.legend(['MDP Policy', 'Selfish Policy', 'SQF Policy', 'Random Policy'])
     plt.show()
     pass
 
 # plot_cost_vs_time()
+plot_cost_cdf_vs_time()
