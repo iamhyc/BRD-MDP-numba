c6e6d6d1c28c01400581d823f4cfcd99f9308555
iamhyc <sudofree@163.com>
2020-05-05 22:09:23 +0800

    temp

--- online_main.py
+++ online_main.py
@@ -61,7 +61,7 @@ def NextState(arrivals, systemStat, oldPolicy, nowPolicy):
                         else:
                             nextStat.ap_stat[k,m,j,xi+1] = lastStat.ap_stat[k,m,j,xi]
 
-        #NOTE: process jobs on ES
+        #NOTE: process jobs on ES #FIXME:
         departures = np.zeros((N_ES, N_JOB), dtype=np.int32)
         for j in range(N_JOB):
             for m in range(N_ES):
@@ -69,7 +69,7 @@ def NextState(arrivals, systemStat, oldPolicy, nowPolicy):
                 if nextStat.es_stat[m,j] > LQ:                  # CLIP [0, LQ]
                     nextStat.es_stat[m,j] = LQ                  #
                 completed_b = toss(1/proc_mean[m,j])            # toss for the first job (if exist)
-                if (nextStat.es_stat[m,j]>=0) and completed_b:   # if first_job_exist and first_job_completed:
+                if (nextStat.es_stat[m,j]>0) and completed_b:   # if first_job_exist and first_job_completed:
                     departures[m,j]       += 1                  #       record departure;
                     nextStat.es_stat[m,j] -= 1                  #       job departure;
                 else:                                           # else:
