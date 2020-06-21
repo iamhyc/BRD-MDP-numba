55dc7764e2157fdd9048f6aa949379426ce603eb
imahyc <sudofree@163.com>
2020-06-21 17:59:51 +0800

    update script

--- scripts/delay-study-vps.sh
+++ scripts/delay-study-vps.sh
@@ -5,14 +5,14 @@ mv -f params.py params.py.bak
 
 # submit full-range delay support task
 mv -f __test__/params-delay-full.py params.py && \
-bsub -q short -n 40 -R "span[ptile=40]" -e %J-full.err -o %J-full.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix full-delay"
+bsub -q short -n 40 -R "span[ptile=40]" -e %J-full.err -o %J-full.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-full"
 sleep 20
 
 # submit normal-range delay support task
 mv -f __test__/params-delay-normal.py params.py && \
-bsub -q short -n 40 -R "span[ptile=40]" -e %J-normal.err -o %J-normal.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix normal-delay"
+bsub -q short -n 40 -R "span[ptile=40]" -e %J-normal.err -o %J-normal.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-normal"
 sleep 20
 
 # submit fixed delay task
 mv -f __test__/params-delay-fixed.py params.py && \
-bsub -q short -n 40 -R "span[ptile=40]" -e %J-fixed.err -o %J-fixed.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix fixed-delay"
+bsub -q short -n 40 -R "span[ptile=40]" -e %J-fixed.err -o %J-fixed.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-fixed"
