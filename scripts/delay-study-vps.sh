a8455dd2e1d9e22b2a7286d5a04c9c82cf412ff8
imahyc <sudofree@163.com>
2020-06-21 22:17:05 +0800

    update scripts

--- scripts/delay-study-vps.sh
+++ scripts/delay-study-vps.sh
@@ -4,15 +4,15 @@
 mv -f params.py params.py.bak
 
 # submit full-range delay support task
-mv -f __test__/params-delay-full.py params.py && \
+cp -f __test__/params-delay-full.py params.py && \
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-full.err -o %J-full.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-full"
 sleep 40
 
 # submit normal-range delay support task
-mv -f __test__/params-delay-normal.py params.py && \
+cp -f __test__/params-delay-normal.py params.py && \
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-normal.err -o %J-normal.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-normal"
 sleep 40
 
 # submit fixed delay task
-mv -f __test__/params-delay-fixed.py params.py && \
+cp -f __test__/params-delay-fixed.py params.py && \
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-fixed.err -o %J-fixed.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-fixed"
