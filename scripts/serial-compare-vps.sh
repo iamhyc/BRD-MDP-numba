e3dcc037641acf1c4b0ddacd3985b1f66ee37b4d
imahyc <sudofree@163.com>
2020-06-21 16:35:18 +0800

    add sleep gap in vps script

--- scripts/serial-compare-vps.sh
+++ scripts/serial-compare-vps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-ser.err -o %J-ser.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --serial-optimize --postfix serial"
-
+sleep 20
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-para.err -o %J-para.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix parallel"
