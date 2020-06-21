2e0c9f3a11622ee6ff3b5a91a22234dff146f813
imahyc <sudofree@163.com>
2020-06-21 21:48:46 +0800

    update script

---
+++ scripts/arrival-study-vps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# WARNING: Execute this script from parent folder.
+
+mv -f params.py params.py.bak
+
+#TODO: submit 1-st arrival-study task
+mv -f __test__/params-arrival-1st.py params.py && \
+bsub -q short -n 40 -R "span[ptile=40]" -e %J-arrival1.err -o %J-arrival1.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix arrival-1st"
+sleep 40
---
+++ scripts/proc-study-vps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# WARNING: Execute this script from parent folder.
+
+mv -f params.py params.py.bak
+
+#TODO: submit 1-st processing-study task
