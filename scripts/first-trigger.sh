b60cb4ffdd5b0d972a8a1ee3a413fae1b7023fce
iamhyc <sudofree@163.com>
2020-06-23 00:15:02 +0800

    fix typo

--- scripts/first-trigger.sh
+++ scripts/first-trigger.sh
@@ -13,11 +13,11 @@ cp -f params.py __test__/params-arrival-2.50.py #6
 cp -f params.py __test__/params-arrival-2.70.py #7
 cp -f params.py __test__/params-arrival-2.90.py #8
 
-cp -f params.py __test__/params-arrival-10-15.py #1
-cp -f params.py __test__/params-arrival-15-20.py #2
-cp -f params.py __test__/params-arrival-20-25.py #3
-cp -f params.py __test__/params-arrival-25-30.py #4
-cp -f params.py __test__/params-arrival-30-35.py #5
-cp -f params.py __test__/params-arrival-35-40.py #6
+cp -f params.py __test__/params-proc-10-15.py #1
+cp -f params.py __test__/params-proc-15-20.py #2
+cp -f params.py __test__/params-proc-20-25.py #3
+cp -f params.py __test__/params-proc-25-30.py #4
+cp -f params.py __test__/params-proc-30-35.py #5
+cp -f params.py __test__/params-proc-35-40.py #6
 
 echo "Folder __test__ Intialized."
