0caf81d55e8a7a14061a3188c26e56b452afd3cf
imahyc <sudofree@163.com>
2020-07-01 15:48:44 +0800

    update output prefix

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 all:
 	pip3 install numpy numba scipy matplotlib PyQt5 termcolor parse --user -i https://mirrors.sustc.us/pypi/simple
 	pip3 install networkx --user
+	pip3 install parse --user
 
 run:
 	@python3 ./online_main.py --postfix test --plot
--- cook-data-trace.py
+++ cook-data-trace.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 import csv
+from parse import parse
 from sys import argv
+from os import path
 
-file_path = argv[1]
-output_dir= argv[2] if argv[2] else './data' 
+_format = '{}-of-{}.csv'
+data_file_path  = argv[1]
+output_dir      = argv[2] if argv[2] else './data'
+_prefix         = parse(_format, data_file_path)[0]
+cooked_file_path= path.join(output_dir, _prefix, '.txt')
 
-with open('file_path', newline='') as fd:
-    csvreader = csv.reader(fd, delimiter=',', quotechar='|')
+with open(data_file_path, newline='') as fin, open(cooked_file_path, 'w') as fout:
+    datatrace = csv.reader(fin, delimiter=',', quotechar='|')
+    
+    num = 0
+    for data in datatrace:
+        num += 1
+        pass
     pass
