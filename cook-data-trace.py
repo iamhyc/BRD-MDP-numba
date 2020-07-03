2d4d5d2a279b445ea16cbc75c660d5add054fac2
imahyc <sudofree@163.com>
2020-07-03 12:23:09 +0800

    temp update

--- cook-data-trace.py
+++ cook-data-trace.py
@@ -3,17 +3,19 @@ import csv
 from parse import parse
 from sys import argv
 from os import path
+import argparse
 
+input_dir       = '../google-datatrace/task_usage/'
+output_dir      = './data'
 _format = '{}-of-{}.csv'
 data_file_path  = argv[1]
-output_dir      = argv[2] if argv[2] else './data'
 _prefix         = parse(_format, data_file_path)[0]
 cooked_file_path= path.join(output_dir, _prefix, '.txt')
 
 with open(data_file_path, newline='') as fin, open(cooked_file_path, 'w') as fout:
+    num = 0
     datatrace = csv.reader(fin, delimiter=',', quotechar='|')
     
-    num = 0
     for data in datatrace:
         num += 1
         pass
