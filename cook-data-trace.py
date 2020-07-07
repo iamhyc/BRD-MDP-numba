cfc2fe898d9fc7ebe056a84065d293875a58c4ee
imahyc <sudofree@163.com>
2020-07-08 00:54:18 +0800

    temp 0054

--- cook-data-trace.py
+++ cook-data-trace.py
@@ -3,20 +3,31 @@ import csv
 from parse import parse
 from sys import argv
 from os import path
-import argparse
+import glob
 
-input_dir       = '../google-datatrace/task_usage/'
+selected_idxs   = range(3)
+input_dir       = argv[1] if len(argv)>2 else '../google-datatrace/task_usage/'
 output_dir      = './data'
-_format = '{}-of-{}.csv'
-data_file_path  = argv[1]
-_prefix         = parse(_format, data_file_path)[0]
-cooked_file_path= path.join(output_dir, _prefix, '.txt')
+_format         = '{}-of-{}.csv'
+trace_files     = glob.glob( path.join(input_dir,'*.csv') )
+trace_files.sort()
 
-with open(data_file_path, newline='') as fin, open(cooked_file_path, 'w') as fout:
-    num = 0
-    datatrace = csv.reader(fin, delimiter=',', quotechar='|')
-    
-    for data in datatrace:
-        num += 1
+for idx in selected_idxs:
+    # generate prefix for current trace file
+    trace   = trace_files[idx]
+    _prefix = path.join( parse(_format, trace)[0] )
+    _prefix = path.join(output_dir, _prefix)
+    trace_file = _prefix+'-trace.txt'; proc_file = _prefix+'-proc.txt'
+    # iteratively reading csv file and summarize
+    with open(trace, newline='') as fin, open(trace_file, 'w') as f_trace, open(proc_file, 'w') as f_proc:
+        datatrace = csv.reader(fin, delimiter=',')
+        for data in datatrace:
+            #TODO: iteratively processing
+            pass
         pass
     pass
+
+# with open(data_file_path, newline='') as fin, open(cooked_file_path, 'w') as fout:
+#     num = 0
+#     datatrace = csv.reader(fin, delimiter=',')
+#     pass
