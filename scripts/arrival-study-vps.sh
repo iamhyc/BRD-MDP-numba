467c27afc6725aa4b4f4261b8ab372a40f2efd52
imahyc <sudofree@163.com>
2020-07-13 22:06:53 +0800

    update all scripts

--- scripts/arrival-study-vps.sh
+++ scripts/arrival-study-vps.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # WARNING: Execute this script from parent folder.
 
-# submit 1x arrival-study task
-bsub -q short -n 40 -R "span[ptile=40]" -e %J-arrival.err -o %J-arrival.out 'NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix arrival-1x\
-                        --inject "TRACE_FOLDER=\"./data/trace-00000-1x\"; arr_prob=np.load(Path(TRACE_FOLDER, \"statistics\"))"'
-
 # submit 1/2x arrival-study task
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-arrival.err -o %J-arrival.out 'NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix arrival-1_2x\
                         --inject "TRACE_FOLDER=\"./data/trace-00000-1_2x\"; arr_prob=np.load(Path(TRACE_FOLDER, \"statistics\"))"'
@@ -20,3 +16,7 @@ bsub -q short -n 40 -R "span[ptile=40]" -e %J-arrival.err -o %J-arrival.out 'NUM
 # submit 1/5x arrival-study task
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-arrival.err -o %J-arrival.out 'NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix arrival-1_5x\
                         --inject "TRACE_FOLDER=\"./data/trace-00000-1_5x\"; arr_prob=np.load(Path(TRACE_FOLDER, \"statistics\"))"'
+
+# submit 1/6x arrival-study task
+bsub -q short -n 40 -R "span[ptile=40]" -e %J-arrival.err -o %J-arrival.out 'NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix arrival-1_6x\
+                        --inject "TRACE_FOLDER=\"./data/trace-00000-1_6x\"; arr_prob=np.load(Path(TRACE_FOLDER, \"statistics\"))"'
--- scripts/delay-study-vps.sh
+++ scripts/delay-study-vps.sh
@@ -6,7 +6,7 @@ bsub -q short -n 40 -R "span[ptile=40]" -e %J-full.err -o %J-full.out "NUMBA_NUM
                         --inject 'BR_MIN=int(1.00*N_SLT-1); BR_MAX=int(1.00*N_SLT); br_dist=genDelayDistribution(True)'"
 # submit normal-range delay support task
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-normal.err -o %J-normal.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-normal \
-                        --inject 'BR_MIN=int(0.70*N_SLT); BR_MAX=int(1.00*N_SLT); br_dist=genDelayDistribution(True)'"
+                        --inject 'BR_MIN=int(0.70*N_SLT-1); BR_MAX=int(0.70*N_SLT); br_dist=genDelayDistribution(True)'"
 # submit medium delay task
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-fixed.err -o %J-fixed.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-medium \
                         --inject 'BR_MIN=int(0.50*N_SLT-1); BR_MAX=int(0.50*N_SLT); br_dist=genDelayDistribution(True)'"
