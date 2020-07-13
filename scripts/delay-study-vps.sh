43d42617a22e2e98337e313d30af82879f40e876
imahyc <sudofree@163.com>
2020-07-13 16:13:01 +0800

    fix delay-script

--- scripts/delay-study-vps.sh
+++ scripts/delay-study-vps.sh
@@ -3,13 +3,13 @@
 
 # submit large delay support task
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-full.err -o %J-full.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-large \
-                        --inject 'BR_MIN=int(1.00*N_SLT); BR_MAX=int(1.00*N_SLT); br_dist=genDelayDistribution(True)'"
+                        --inject 'BR_MIN=int(1.00*N_SLT-1); BR_MAX=int(1.00*N_SLT); br_dist=genDelayDistribution(True)'"
 # submit normal-range delay support task
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-normal.err -o %J-normal.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-normal \
                         --inject 'BR_MIN=int(0.70*N_SLT); BR_MAX=int(1.00*N_SLT); br_dist=genDelayDistribution(True)'"
 # submit medium delay task
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-fixed.err -o %J-fixed.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-medium \
-                        --inject 'BR_MIN=int(0.50*N_SLT); BR_MAX=int(0.50*N_SLT); br_dist=genDelayDistribution(True)'"
+                        --inject 'BR_MIN=int(0.50*N_SLT-1); BR_MAX=int(0.50*N_SLT); br_dist=genDelayDistribution(True)'"
                         # submit medium delay task
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-fixed.err -o %J-fixed.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix delay-small \
-                        --inject 'BR_MIN=int(0.10*N_SLT); BR_MAX=int(0.10*N_SLT); br_dist=genDelayDistribution(True)'"
+                        --inject 'BR_MIN=int(0.10*N_SLT-1); BR_MAX=int(0.10*N_SLT); br_dist=genDelayDistribution(True)'"
