993dd561ba6e97c4b7dddf0cbabcb784191b766f
imahyc <sudofree@163.com>
2020-07-14 19:53:01 +0800

    temp 1952

--- scripts/proc-study-vps.sh
+++ scripts/proc-study-vps.sh
@@ -30,17 +30,17 @@ bsub -q short -n 40 -R "span[ptile=40]" -e %J-proc-2.00x.err -o %J-proc.out "NUM
                         --inject 'PROC_MIN=int(1.75*N_SLT); PROC_MAX=int(2.00*N_SLT); proc_mean=genProcessingParameter(es2ap_map,True)'"
 
 # submit 2.25x processing-study task
-bsub -q short -n 40 -R "span[ptile=40]" -e %J-proc-2.25x.err -o %J-proc.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix proc-2.25x \
-                        --inject 'PROC_MIN=int(2.00*N_SLT); PROC_MAX=int(2.25*N_SLT); proc_mean=genProcessingParameter(es2ap_map,True)'"
+# bsub -q short -n 40 -R "span[ptile=40]" -e %J-proc-2.25x.err -o %J-proc.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix proc-2.25x \
+#                         --inject 'PROC_MIN=int(2.00*N_SLT); PROC_MAX=int(2.25*N_SLT); proc_mean=genProcessingParameter(es2ap_map,True)'"
 
 # submit 2.50x processing-study task
-bsub -q short -n 40 -R "span[ptile=40]" -e %J-proc-2.50x.err -o %J-proc.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix proc-2.50x \
-                        --inject 'PROC_MIN=int(2.25*N_SLT); PROC_MAX=int(2.50*N_SLT); proc_mean=genProcessingParameter(es2ap_map,True)'"
+# bsub -q short -n 40 -R "span[ptile=40]" -e %J-proc-2.50x.err -o %J-proc.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix proc-2.50x \
+#                         --inject 'PROC_MIN=int(2.25*N_SLT); PROC_MAX=int(2.50*N_SLT); proc_mean=genProcessingParameter(es2ap_map,True)'"
 
 #submit 2.75x processing-study task
-bsub -q short -n 40 -R "span[ptile=40]" -e %J-proc-2.75x.err -o %J-proc.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix proc-2.75x \
-                        --inject 'PROC_MIN=int(2.50*N_SLT); PROC_MAX=int(2.75*N_SLT); proc_mean=genProcessingParameter(es2ap_map,True)'"
+# bsub -q short -n 40 -R "span[ptile=40]" -e %J-proc-2.75x.err -o %J-proc.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix proc-2.75x \
+#                         --inject 'PROC_MIN=int(2.50*N_SLT); PROC_MAX=int(2.75*N_SLT); proc_mean=genProcessingParameter(es2ap_map,True)'"
 
 #submit 3.00x processing-study task
-bsub -q short -n 40 -R "span[ptile=40]" -e %J-proc-3.00x.err -o %J-proc.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix proc-3.00x \
-                        --inject 'PROC_MIN=int(2.75*N_SLT); PROC_MAX=int(3.00*N_SLT); proc_mean=genProcessingParameter(es2ap_map,True)'"
+# bsub -q short -n 40 -R "span[ptile=40]" -e %J-proc-3.00x.err -o %J-proc.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix proc-3.00x \
+#                         --inject 'PROC_MIN=int(2.75*N_SLT); PROC_MAX=int(3.00*N_SLT); proc_mean=genProcessingParameter(es2ap_map,True)'"
