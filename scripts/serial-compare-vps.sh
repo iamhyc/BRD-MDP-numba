6080e2e884b1519cef69f65183b70d0880e00da5
imahyc <sudofree@163.com>
2020-09-21 21:28:53 +0800

    short sleep interval

--- scripts/serial-compare-vps.sh
+++ scripts/serial-compare-vps.sh
@@ -2,5 +2,5 @@
 # WARNING: Execute this script from parent folder.
 
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-ser.err -o %J-ser.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --serial-optimize --postfix serial"
-sleep 40
+sleep 10
 bsub -q short -n 40 -R "span[ptile=40]" -e %J-para.err -o %J-para.out "NUMBA_NUM_THREADS=40 python3 ./online_main.py --postfix parallel"
