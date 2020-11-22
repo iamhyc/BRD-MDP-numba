9ed49ec375b79d5d5c96a9640f9cce5dbf1416b9
iamhyc <sudofree@163.com>
2020-11-22 17:06:36 +0800

    some fix

--- plot-traces2.py
+++ plot-traces2.py
@@ -196,6 +196,8 @@ def plot_tight_bound():
     # plt.plot(EVAL_RANGE, r_mdp, '.r-')
     # plt.plot(EVAL_RANGE, r_ti,  '.b-')
     plt.plot(EVAL_RANGE, np.array(r_ti)-np.array(r_mdp), '.k-')
+    plt.xlabel('T')
+    plt.ylabel('Performance Gap (Numerical Part)')
     plt.show()
     pass
 
